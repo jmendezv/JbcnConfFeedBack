10907e6962aafc77ff084f12ad5cbd43140da6c6
Pep Mendez <jmendez1@xtec.cat>
2018-06-26 14:15:24 +0200

    Added a new Application class to the AndroidManifest.xml file

---
+++ app/src/main/java/cat/cristina/pep/jbcnconffeedback/application/JbcnConfFeedBackApplication.kt
@@ -0,0 +1,10 @@
+package cat.cristina.pep.jbcnconffeedback.application
+
+import android.app.Application
+
+class JbcnConfFeedBackApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
